2638dfc5b5c69f9e6a01914be237ee05848877e6
drewjya <andre.wijaya004@binus.ac.id>
2022-12-12 23:02:50 +0700

    upd

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -12,19 +12,10 @@ const firebaseConfig = { // Have the firebase config here
     appId: import.meta.env.VITE_MESSAGING_APP_ID,
 };
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyBQ1C4O_nPkPzzr35iVZbwypuSNWm-UW9U",
-//     authDomain: "testing-b7d59.firebaseapp.com",
-//     projectId: "testing-b7d59",
-//     storageBucket: "testing-b7d59.appspot.com",
-//     messagingSenderId: "377561562113",
-//     appId: "1:377561562113:web:fa832b39fbe3e7688e5400",
-//     measurementId: "G-17BFQ5HXB2"
-// };
-// Use this to initialize the firebase App
+
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
-// Use these for db & auth
+
 const firestore = firebaseApp.firestore();
 const auth = firebase.auth();
 
